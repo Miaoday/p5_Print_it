d24dd837dd3ca282839205e780425a3b334cce70
Miaoday <Starryvero@gmail.com>
2023-10-29 23:53:19 +0100

    js update slidephotoadded.

--- assets/script.js
+++ assets/script.js
@@ -1,66 +1,4 @@
-
-
-const addImage = document.createElement('img');
-addImage.source = 	'./assets/images/slideshow/slide2.jpg';
-					'./assets/images/slideshow/slide3.jpg',
-	 				'./assets/images/slideshow/slide4.png', 
-
-addImage.className = 'banner-image';
-const track = document.getElementById('banner');
-const slideShow = Array.from(track.children);
-
-const slideWidth = slideShow[0].getBoundingClientRect().width;
-
-console.log(addImage);
-
-
-const imagePath = [
-	 
-	 
- ]
- imagePath.forEach(image => {
-	const addImage = document.createElement('img');
-	image.source = imagePath;
-	addImage.className = 'banner-image';
-	const carrousel = document.getElementById('banner');
-
- })
-
-
-const leftButton = document.querySelector('.arrow_left');
-const rightButton = document.querySelector('.arrow_right');
-const dotsBar = document.querySelector('.dots');
-const dots= Array.from(dotsBar.children);
-console.log(dots);
-
-//const slideSize = slideShow[0].getBoundingClientRect();
-//const slideWidth = slideSize.width;
-// arrange the width of each slide
-// arrange the slides next to the other slide
-// slideShow[0].style.left = slideWidth * 0 + 'px';
-// slideShow[1].style.left = slideWidth * 1 + 'px';
-// slideShow[2].style.left = slideWidth * 2 + 'px';
-// slideShow[3].style.left = slideWidth * 3 + 'px';
-const slideShowPosition = (slide, index) => {
-	slide.style.left = slideWidth * index + 'px';
-};
-slideShow.forEach(slideShowPosition);
-// when I click on the left button the slide move to the left
-// when I click on the right button the slide move to the right
-rightButton.addEventListener('click', e => {
-	// move the slide to the right
-	const currentSlide = track.querySelector('.current_slide');
-	// continue move to the next slide if we clicke
-	const nextSlide = currentSlide.nextElementSibling;
-	const slideToLeft = nextSlide.style.left;
-	track.style.transform = 'translate(-' + slideToLeft +')';
-	// track.style.transform = 'translat'('-' + 'slideToLeft'); pourquoi pas ecrit comme ca 
-	currentSlide.classList.remove('current_slide');
-	nextSlide.classList.add('current_slide');
-})
-
 const slides = [
-
 	{
 		"image":"slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
@@ -79,3 +17,83 @@ const slides = [
 	}
 ]
 console.log(slides);
+
+// Add all images to the track
+const track = document.getElementById('banner');
+const tagLine = document.querySelector('p');
+const firstSlide = track.children[0];
+console.log(firstSlide);
+
+for (let i = 1; i< slides.length; i++) {
+   const addImage = document.createElement('img');
+   addImage.classList.add('banner-img');
+   addImage.src = `./assets/images/slideshow/${slides[i].image}`;
+   addImage.alt = 'Banner Print-it';  
+   track.appendChild(addImage, firstSlide);
+   track.insertBefore(addImage, tagLine);
+}
+
+
+const leftButton = document.querySelector('.arrow_left');
+const rightButton = document.querySelector('.arrow_right');
+
+leftButton.addEventListener('click', (e) => {
+   console.log("clicked on the left button");
+   // when I click on the left button the slide move to the left
+
+})
+rightButton.addEventListener('click', (e) => {
+   console.log("clicked on the right button");
+   // when I click on the right button the slide move to the right
+
+})
+// get the number of slides and connect the to the dots container
+// create the slides of dots
+
+function createDots() {
+   const dotsContainer = document.querySelector('.dots'); 
+   dotsContainer.innerHTML = '<div class="dots"></div>';
+   
+   for (let i = 0; i < slides.length; i++) {
+      const dot = document.createElement('div');
+      dot.classList.add('dot');
+      dotsContainer.appendChild(dot);
+   // Add the selected class to the current slide
+      if (i === 0) {
+         
+      }
+   }
+}
+// Appel de la fonction createDots
+createDots();
+
+let currentSlideIndex = 0;
+
+// update the dots when I click on the dot
+function updateDots() {
+   
+   const allDots = document.querySelectorAll('.dot')
+
+   allDots.forEach(dot => {
+      dot.classList.remove('dot_selected');
+   });
+   allDots[currentSlideIndex].classList.add('dot_selected');
+}
+
+updateDots();
+
+// update the photo when I click on the dot 
+function updateSlide() {
+   
+   const slideShow = document.querySelectorAll('.banner-image');
+   
+
+   // update the image according to the currentSlideIndex 
+   slideShow.forEach((slide, index) => {
+     slide.style.left = `${(index - currentSlideIndex) * 100}%`;
+   });
+ 
+   // update the tagLing text according to the currentSlideIndex
+   
+}
+
